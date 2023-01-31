f48dd93681a0443f26ec4d8d92872ac073fbfe1e
joauluiz <joao_lmm@hotmail.com>
2023-01-31 15:25:41 -0300

    testes API após encontro 4

--- api.py
+++ api.py
@@ -1,23 +1,49 @@
-from fastapi import FastAPI, Depends
+import asyncio
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from starlette import status
 import mlp
 from entities.model_parameters import Model_Parameters
 from entities.model_parameters_api import Model_Inputs_Api
 from entities.user_credentials import User_Credentials
 from user import data_norm
-from fastapi_users import FastAPIUsers
-from fastapi_users.authentication.basic import BasicAuth
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
 app = FastAPI()
 
-
-def validate_credentials(credentials: User_Credentials):
-    if credentials.user_name == "admin" and credentials.password == "123":
-        return True
-    return False
+security = HTTPBasic()
 
 
-basic_auth = BasicAuth(validate_user=validate_credentials(User_Credentials()))
-fastapi_users = FastAPIUsers(app, auth_backend=basic_auth)
+@app.get("/users/me")
+# def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+#     return {"username": credentials.username, "password": credentials.password}
+#
+#
+# def validate_credentials(credentials: User_Credentials):
+#     if credentials.user_name == "admin" and credentials.password == "123":
+#         return True
+#     return False
+async def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = b"admin"
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = b"123"
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    return "Released access"
 
 
 def output_message(number_output):
@@ -30,7 +56,8 @@ def output_message(number_output):
 
 
 @app.post("/")
-def out_model(parameters: Model_Inputs_Api):
+async def out_model(parameters: Model_Inputs_Api, user = Depends(get_current_username)):
+
     train_output_norm, train_input_norm, \
         test_output_norm, test_input_norm = data_norm()
 
